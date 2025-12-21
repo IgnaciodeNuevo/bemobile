390b4049a83bc601156fe0784ff2e3e698f7b598
Kay <ignaciodenuevo@posteo.net>
2025-12-21 13:37:43 +0100

    Add header.scss and update Header.tsx to add styles

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,6 +1,7 @@
 import { Link } from 'react-router-dom'
 import { useFavorites } from '@/context/index'
 import { Logo } from '@/components/Logo/index'
+import './Header.scss'
 
 interface HeaderProps {
   onFavoritesClick?: () => void
@@ -10,16 +11,17 @@ export function Header({ onFavoritesClick }: HeaderProps) {
   const { favorites } = useFavorites()
 
   return (
-    <header>
-      <Link to="/" >
+    <header className="header">
+      <Link to="/">
         <Logo />
       </Link>
       <button
+        className="header__favorites"
         onClick={onFavoritesClick}
         aria-label={`Favoritos: ${favorites.length} personajes`}
       >
-        <span>♥</span>
-        <span>{favorites.length}</span>
+        <span className="header__favorites-icon">♥</span>
+        <span className="header__favorites-count">{favorites.length}</span>
       </button>
     </header>
   )
