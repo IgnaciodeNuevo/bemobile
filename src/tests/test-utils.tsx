d12447f12cfaae12204e3eb91115ae861ff09e8f
Kay <ignaciodenuevo@posteo.net>
2025-12-21 10:41:16 +0100

    Add test-utils.tsx

---
+++ src/tests/test-utils.tsx
@@ -0,0 +1,18 @@
+import { ReactElement } from 'react'
+import { render, RenderOptions } from '@testing-library/react'
+import userEvent from '@testing-library/user-event'
+
+const AllProviders = ({ children }: { children: React.ReactNode }) => (
+  <>{children}</>
+)
+
+const customRender = (
+  ui: ReactElement,
+  options?: Omit<RenderOptions, 'wrapper'>
+) => ({
+  user: userEvent.setup(),
+  ...render(ui, { wrapper: AllProviders, ...options }),
+})
+
+export * from '@testing-library/react'
+export { customRender as render }
