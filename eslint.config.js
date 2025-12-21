16259dd3dbb8e0cda4c930e968b001649bbd4bab
Kay <ignaciodenuevo@posteo.net>
2025-12-21 10:39:45 +0100

    Add ignore to tests

--- eslint.config.js
+++ eslint.config.js
@@ -6,7 +6,7 @@ import tseslint from 'typescript-eslint'
 import { defineConfig, globalIgnores } from 'eslint/config'
 
 export default defineConfig([
-  globalIgnores(['dist']),
+  globalIgnores(['dist', 'src/tests/**']),
   {
     files: ['**/*.{ts,tsx}'],
     extends: [
