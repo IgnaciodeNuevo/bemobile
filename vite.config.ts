ac738f99a24ad1a36d7430deaa5983e195c78d6a
Kay <ignaciodenuevo@posteo.net>
2025-12-23 15:48:59 +0100

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -37,13 +37,7 @@ export default defineConfig({
     coverage: {
       provider: 'v8',
       reporter: ['text', 'json', 'html'],
-      exclude: [
-        'node_modules/',
-        'src/tests/setup.ts',
-        '**/*.d.ts',
-        '**/*.config.*',
-        '**/main.tsx',
-      ],
+      exclude: ['node_modules/', 'src/tests/setup.ts', '**/*.d.ts', '**/*.config.*', '**/main.tsx'],
     },
   },
 })
